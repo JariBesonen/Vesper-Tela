6c2800e493add95e07878096ac8d848868cceda5
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2026-03-18 21:48:47 -0700

    Add navbar feature

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,18 @@
-import "./Navbar.css";
-
+import './Navbar.css';
 function Navbar() {
-  return (
-    <nav>
-      <ul className="ul-left">
-        <li>
-          <a href="/men">Men</a>
-        </li>
-        <li>
-          <a href="/woman">Woman</a>
-        </li>
-      </ul>
-      <h1>Vesper Tela</h1>
-      <ul className="ul-right">
-        <li>
-          <a href="/cart">Cart</a>
-        </li>
-        <li>
-          <a href="/saved">Saved</a>
-        </li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav>
+            <ul className="ul-left">
+                <li><a href="/men">Men</a></li>
+                <li><a href="/woman">Woman</a></li>
+            </ul>
+            <h1>Vesper Tela</h1>
+            <ul className="ul-right">
+                <li><a href="/cart">Cart</a></li>
+                <li><a href="/saved">Saved</a></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar;
