d4ca79222858ea7a6b7abccdf9d4f28d647bb77e
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2026-03-18 21:03:31 -0700

    Install react-router-dom in frontend and remove package.json/package-lock.json from root

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
+import "./Navbar.css";
+
 function Navbar() {
-    return (
-        <></>
-    )
+  return (
+    <nav>
+      <ul className="ul-left">
+        <li>
+          <a href="/men">Men</a>
+        </li>
+        <li>
+          <a href="/woman">Woman</a>
+        </li>
+      </ul>
+      <h1>Vesper Tela</h1>
+      <ul className="ul-right">
+        <li>
+          <a href="/cart">Cart</a>
+        </li>
+        <li>
+          <a href="/saved">Saved</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
