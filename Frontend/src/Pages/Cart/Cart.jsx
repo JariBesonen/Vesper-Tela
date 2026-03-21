57a3a4ac0a9174dd6c4c70fc7e22cdde3e4c1114
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2026-03-21 15:34:11 -0700

    setup basic cart page structure

--- Frontend/src/Pages/Cart/Cart.jsx
+++ Frontend/src/Pages/Cart/Cart.jsx
@@ -1,7 +1,36 @@
+import "./Cart.css";
+
 function Cart() {
-    return (
-        <></>
-    )
+  return (
+    <div className="cart-page">
+      <div className="cart-wrapper">
+        <div className="cart-item">
+          <div className="item-img">IMG</div>
+          <span className="item-name">NAME</span>
+          <span className="item-price">PRICE</span>
+          <button className="remove-from-cart-btn">REMOVE FROM CART</button>
+        </div>
+        <div className="cart-item">
+          <div className="item-img">IMG</div>
+          <span className="item-name">NAME</span>
+          <span className="item-price">PRICE</span>
+          <button className="remove-from-cart-btn">REMOVE FROM CART</button>
+        </div>
+        <div className="cart-item">
+          <div className="item-img">IMG</div>
+          <span className="item-name">NAME</span>
+          <span className="item-price">PRICE</span>
+          <button className="remove-from-cart-btn">REMOVE FROM CART</button>
+        </div>
+        <div className="proceed-to-checkout-wrapper">
+            <span>PRICE</span>
+          <button className="proceed-to-checkout-btn">
+            PROCEED TO CHECKOUT
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
