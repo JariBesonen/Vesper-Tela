5dff69192845deed99a564f8124af81614af3b54
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2026-03-21 09:05:52 -0700

    Setup boilerplate for mens page

--- Frontend/src/Pages/Men/Men.jsx
+++ Frontend/src/Pages/Men/Men.jsx
@@ -1,6 +1,40 @@
+import "./Men.css";
+
 function Men() {
-    return(
-        <></>
-    )
+  return (
+    <div className="mens-page">
+      <div className="mens-product-wrapper">
+        <span className="product-wrapper-save-icon">save</span>
+        <div className="product-info-wrapper">
+          <span className="product-name">PRODUCT NAME</span>
+          <span className="product-price">PRICE</span>
+        </div>
+      </div>
+
+      <div className="mens-product-wrapper">
+        <span className="product-wrapper-save-icon">save</span>
+        <div className="product-info-wrapper">
+          <span className="product-name">PRODUCT NAME</span>
+          <span className="product-price">PRICE</span>
+        </div>
+      </div>
+
+      <div className="mens-product-wrapper">
+        <span className="product-wrapper-save-icon">save</span>
+        <div className="product-info-wrapper">
+          <span className="product-name">PRODUCT NAME</span>
+          <span className="product-price">PRICE</span>
+        </div>
+      </div>
+      <div className="mens-product-wrapper">
+        <span className="product-wrapper-save-icon">save</span>
+        <div className="product-info-wrapper">
+          <span className="product-name">PRODUCT NAME</span>
+          <span className="product-price">PRICE</span>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Men;
