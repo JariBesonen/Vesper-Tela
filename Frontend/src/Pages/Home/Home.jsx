115eb66b4a905bf058372c4591cc8309f2763452
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2026-05-20 14:12:16 -0700

    Add mobile hero image support - display image on mobile, video on desktop

--- Frontend/src/Pages/Home/Home.jsx
+++ Frontend/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.css";
 import { Link, useNavigate } from "react-router-dom";
 import heroVideo from "../../assets/productImgs/heroVideo.mp4";
+import heroImageMobile from "../../assets/productImgs/heroImageMobile.jpg";
 import menSectionBackground from "../../assets/productImgs/heropageMenCTA.png";
 import womenShoesCtaImage from "../../assets/productImgs/womansCoffeeHeels.png";
 import womenShirtsCtaImage from "../../assets/productImgs/womansBeigeLongSleeveShirt.png";
@@ -14,7 +15,10 @@ function Home() {
 
   return (
     <main>
-      <section className="hero-section">
+      <section 
+        className="hero-section"
+        style={{ backgroundImage: `url(${heroImageMobile})` }}
+      >
         <video
           className="hero-video"
           autoPlay
